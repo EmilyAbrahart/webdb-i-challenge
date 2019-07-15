1fa064e1d16344fe5ca5352b312fc0c3e757dc42
EmilyAbrahart <emilyabrahart@gmail.com>
2019-07-15 13:25:16 +0100

    db helper functions written out

--- server.js
+++ server.js
@@ -6,4 +6,28 @@ const server = express();
 
 server.use(express.json());
 
+function getAllAccounts() {
+	return db('accounts');
+}
+
+function getAccountById(id) {
+	return db('accounts').where({ id });
+}
+
+function createNewAccount({ name, budget }) {
+	return db('accounts').insert({ name, budget });
+}
+
+function updateAccountById(id, { name, budget }) {
+	return db('accounts')
+		.where({ id })
+		.update({ name, budget });
+}
+
+function deleteAccountById(id) {
+	return db('accounts')
+		.where({ id })
+		.del();
+}
+
 module.exports = server;
